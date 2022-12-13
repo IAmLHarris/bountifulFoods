153efd533bf28bb3c824e2442fbf8144594585e3
IAmLiamH <har21091@byui.edu>
2022-12-13 14:11:32 -0600

    weather 1

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,7 +1,11 @@
 const requestURL =
-  "https://api.openweathermap.org/data/2.5/weather?lat=-33.4569&lon=-70.6483&appid=9e972cb2fc69ed1c08d9704b40ffe53e&units=imperial";
+  "https://api.openweathermap.org/data/2.5/weather?lat=33.1581&lon=-117.3506&appid=9e972cb2fc69ed1c08d9704b40ffe53e&units=imperial";
+
+const requestURL2 = "https://api.openweathermap.org/data/2.5/forecast?q=Carlsbad&appid=9e972cb2fc69ed1c08d9704b40ffe53e&units=imperial";
+
 const cards = document.querySelector(".buiscards");
 // [-33.4569, -70.6483]
+// 
 
 async function apiFetch() {
   try {
@@ -25,10 +29,12 @@ function displayResults(weatherData) {
 
   const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
   const desc = weatherData.weather[0].description;
+  currentHumi.innerHTML = weatherData.main.humidity.toFixed(0);
 
   weatherIcon.setAttribute("src", iconsrc);
   weatherIcon.setAttribute("alt", desc);
   captionDesc.textContent = desc;
+
 }
 
 apiFetch();
@@ -36,4 +42,22 @@ apiFetch();
 // select HTML elements in the document
 const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon");
-const captionDesc = document.querySelector("figcaption");
+const captionDesc = document.querySelector("#figcaption");
+const currentHumi = document.querySelector("#humidity")
+
+
+async function apiFetch2() {
+  try {
+    const response = await fetch(requestURL2);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
