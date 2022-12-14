1021470fd85dd5598b2e78483abcf17f3a37a5a4
IAmLiamH <har21091@byui.edu>
2022-12-13 23:06:03 -0600

    about us

---
+++ scripts/map.js
@@ -0,0 +1,10 @@
+// AIzaSyDYp8qljvCPRQ9UbzjoCohDUBtQtMti7Ek
+
+// Initialize and add the map
+function myMap() {
+  var mapProp = {
+    center: new google.maps.LatLng(33.1581, -117.35),
+    zoom: 12,
+  };
+  var map = new google.maps.Map(document.getElementById("map"), mapProp);
+}
