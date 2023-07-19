b37774ad6366b24c4de4d522c1b79020c362ca92
IAmLHarris <har21091@byui.edu>
2023-07-18 19:32:19 -0500

    What if?

--- scripts/fruit.js
+++ scripts/fruit.js
@@ -1,8 +1,6 @@
 const requestURL = "./scripts/fruit.json";
 const cards = document.querySelector(".fruitOptions");
 
-console.log("bingus");
-
 fetch(requestURL)
   .then(function (response) {
     return response.json();
@@ -26,6 +24,8 @@ function displayBuisinesses(fruitList) {
 
   fruitn.textContent = fruitList.name;
 
+  fruitn.setAttribute("value", fruitList.name);
+
   // console.log(fruitList.name);
   // console.log(fruitn.textContent);
 
@@ -45,6 +45,7 @@ function displayBuisinesses2(fruitList) {
 
   fruitn.textContent = fruitList.name;
 
+  fruitn.setAttribute("value", fruitList.name);
   // console.log(fruitList.name);
   // console.log(fruitn.textContent);
 
@@ -64,6 +65,7 @@ function displayBuisinesses3(fruitList) {
 
   fruitn.textContent = fruitList.name;
 
+  fruitn.setAttribute("value", fruitList.name);
   // console.log(fruitList.name);
   // console.log(fruitn.textContent);
 
@@ -73,7 +75,54 @@ function displayBuisinesses3(fruitList) {
   document.querySelector("#fruitDrop3").appendChild(fruitn);
 }
 
-function poplol() {
-  preventDefault();
-  console.log("buttpoop");
+const f1 = document.getElementById("fruitDrop");
+const f2 = document.getElementById("fruitDrop2");
+const f3 = document.getElementById("fruitDrop3");
+
+const fruitbox = document.getElementById("fruitBox");
+
+function setorder() {
+  localStorage.setItem("ord1", "");
+  localStorage.setItem("ord2", "");
+  localStorage.setItem("ord3", "");
+
+  while (fruitbox.hasChildNodes()) {
+    fruitbox.removeChild(fruitbox.firstChild);
+  }
+
+  var value1 = f1.value;
+  // var text1 = f1.options[f1.selectedIndex].text;
+
+  var value2 = f2.value;
+  // var text2 = f2.options[f2.selectedIndex].text;
+
+  var value3 = f3.value;
+  // var text3 = f3.options[f3.selectedIndex].text;
+
+  localStorage.setItem("ord1", value1);
+
+  localStorage.setItem("ord2", value2);
+
+  localStorage.setItem("ord3", value3);
+
+  // console.log(value1);
+  // console.log(text1);
+
+  // console.log(value2);
+  // console.log(text2);
+
+  // console.log(value3);
+  // console.log(text3);
+
+  let ord1 = document.createElement("p");
+  ord1.textContent = localStorage.getItem("ord1");
+  document.querySelector("#fruitBox").appendChild(ord1);
+
+  let ord2 = document.createElement("p");
+  ord2.textContent = localStorage.getItem("ord2");
+  document.querySelector("#fruitBox").appendChild(ord2);
+
+  let ord3 = document.createElement("p");
+  ord3.textContent = localStorage.getItem("ord3");
+  document.querySelector("#fruitBox").appendChild(ord3);
 }
---
+++ scripts/fruitbox.js
@@ -0,0 +1,21 @@
+let ord1 = document.createElement("p");
+ord1.textContent = localStorage.getItem("ord1");
+document.querySelector("#fruitBox").appendChild(ord1);
+
+let ord2 = document.createElement("p");
+ord2.textContent = localStorage.getItem("ord2");
+document.querySelector("#fruitBox").appendChild(ord2);
+
+let ord3 = document.createElement("p");
+ord3.textContent = localStorage.getItem("ord3");
+document.querySelector("#fruitBox").appendChild(ord3);
+
+function droporder() {
+  localStorage.setItem("ord1", "");
+  localStorage.setItem("ord2", "");
+  localStorage.setItem("ord3", "");
+
+  while (fruitbox.hasChildNodes()) {
+    fruitbox.removeChild(fruitbox.firstChild);
+  }
+}
